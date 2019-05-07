ca074f32b8b0693044de4b829776f88785e6f02b
Sze Wah Leong <szewahleong@Szes-MacBook-Pro.local>
2019-05-07 15:05:12 -0400

    modified config

--- config/config.js
+++ config/config.js
@@ -15,16 +15,9 @@ module.exports = {
         "host": "127.0.0.1",
         "dialect": "mysql"
     },
-    {
-
+    production: {
+        use_env_variable: "JAWSDB_URL",
+        dialect: "mysql"
     }
 
-
-
-
-
-
-
-
-
-}
+};
